2d39541ac276cc529815cc9e2f083480b9398579
Pavel Konnov <4itonada@gmail.com>
2019-05-16 15:54:03 +0400

    add connect to db

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser =  require('body-parser')
 const authRoutes = require('./routes/auth')
 const orderRoutes = require('./routes/order')
 const categoryRoutes = require('./routes/category')
 const positionRoutes= require('./routes/position')
 const analyticsRoutes = require('./routes/analytics')
+const keys = require('./config/keys')
 const app = express()
 
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('db connect'))
+  .catch(error => console.log(error))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://spermspace:OIJdeZxO@cluster0-fz7tx.mongodb.net/test?retryWrites=true'
+}
