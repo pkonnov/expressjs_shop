f009c649767f9c4947c63bac526cf6d41bbccf7c
Pavel Konnov <4itonada@gmail.com>
2019-05-19 10:25:26 +0400

    the end block auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const keys = require('../config/keys')
 const User = require('../models/User')
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.login = async function(req, res) {
   const candidate = await User.findOne({email: req.body.email})
@@ -51,7 +52,7 @@ module.exports.register = async function(req, res) {
       await user.save()
       res.status(201).json(user)
     } catch(e) {
-      console.log(e)
+      errorHandler(res, e)
     }
   }
 
