e774b215fc2d74349c37d7dc7f4d95da8a2b1e0a
Pavel Konnov <4itonada@gmail.com>
2019-05-21 16:19:36 +0400

    before eject

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <div>Start</div>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
